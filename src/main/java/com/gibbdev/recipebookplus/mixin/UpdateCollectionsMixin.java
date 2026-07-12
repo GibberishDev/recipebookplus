d143255912a14abe756dcf8bc9803f0ffadc50f5
GibberishDev <gibberishdevelopment@gmail.com>
2026-07-13 02:03:48 +0300

    Implement modid search, ingredient search, mod config(prefixes for searches, mod toggle and recipe grouping search)

--- src/main/java/com/gibbdev/recipebookplus/mixin/UpdateCollectionsMixin.java
+++ src/main/java/com/gibbdev/recipebookplus/mixin/UpdateCollectionsMixin.java
@@ -23,6 +23,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -30,10 +31,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
+import javax.annotation.RegEx;
+import java.io.Console;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
 
 @Mixin(RecipeBookComponent.class)
 public class UpdateCollectionsMixin {
@@ -63,26 +65,73 @@ public class UpdateCollectionsMixin {
         List<RecipeCollection> list1 = Lists.newArrayList(list);
         list1.removeIf((collection) -> !collection.hasKnownRecipes());
         list1.removeIf((collection) -> !collection.hasFitting());
-        String s = this.searchBox.getValue();
-        if (!s.isEmpty()) {
-            ClientPacketListener clientpacketlistener = minecraft.getConnection();
-            if (clientpacketlistener != null) {
-                List<RecipeCollection> tempList = Lists.newArrayList(list1);
-                for (RecipeCollection collection : tempList) {
-                    if (collection.getRecipes().size() >= 2) {
-                        for (RecipeHolder<?> holder : collection.getRecipes()) {
-                            RecipeCollection newCollection = new RecipeCollection(minecraft.player.registryAccess(), List.of(holder));
-                            newCollection.canCraft(stackedContents, menu.getGridWidth(), menu.getGridHeight(), this.book);
-                            list1.add(list1.indexOf(collection), newCollection);
-                        }
-                        list1.remove(collection);
+        String s = searchBox.getValue().toLowerCase(Locale.ROOT);
+
+
+        List<RecipeCollection> tempList = Lists.newArrayList(list1);
+        if (Config.SEARCH_UNGROUP.getAsBoolean() && !s.isEmpty()) {
+            for (RecipeCollection collection : tempList) {
+                if (collection.getRecipes().size() >= 2) {
+                    for (RecipeHolder<?> holder : collection.getRecipes()) {
+                        RecipeCollection newCollection = new RecipeCollection(minecraft.player.registryAccess(), List.of(holder));
+                        newCollection.canCraft(stackedContents, menu.getGridWidth(), menu.getGridHeight(), this.book);
+                        list1.add(list1.indexOf(collection), newCollection);
                     }
+                    list1.remove(collection);
                 }
             }
         }
+        if (s.startsWith(Config.INGREDIENT_PREFIX.get()) && s != Config.INGREDIENT_PREFIX.get()) {
+            String searchTerm = s.replaceFirst(Matcher.quoteReplacement(Config.INGREDIENT_PREFIX.get()), "").strip();
+            if (searchTerm != "") {
+                List<ItemStack> itemStacks = searchItems(searchTerm);
+                tempList = Lists.newArrayList(list1);
+                for (RecipeCollection collection : tempList) {
+                    boolean ingredientFound = false;
+                    for (RecipeHolder<?> holder : collection.getRecipes()) {
+                        Recipe<?> recipe = holder.value();
+                        for (Ingredient ingredient : recipe.getIngredients()) {
+                            for (ItemStack item : itemStacks) {
+                                if (ingredient.test(item)) {
+                                    ingredientFound = true;
+                                    break;
+                                }
+                            }
+                            if (ingredientFound) break;
+                        }
+                        if (ingredientFound) break;
+                    }
+                    if (!ingredientFound) list1.remove(collection);
+                }
+            }
+        } else if (s.startsWith(Config.MODID_PREFIX.get()) && s != Config.MODID_PREFIX.get()) {
+            String searchTerm = s.replaceFirst(Matcher.quoteReplacement(Config.MODID_PREFIX.get()), "").strip();
+            if (searchTerm != "") {
+                tempList = Lists.newArrayList(list1);
+                for (RecipeCollection collection : tempList) {
+                    for (RecipeHolder<?> holder : collection.getRecipes()) {
+                        Item resultItem = holder.value().getResultItem(minecraft.player.registryAccess()).getItem();
+                        String namespace = BuiltInRegistries.ITEM.getKey(resultItem).getNamespace();
+                        if (!namespace.contains(searchTerm)) list1.remove(collection);
+                    }
+                }
+            }
 
-        if (this.book.isFiltering(this.menu)) {
-            list1.removeIf((p_100331_) -> !p_100331_.hasCraftable());
+        } else {
+            tempList = Lists.newArrayList(list1);
+            for (RecipeCollection collection : tempList) {
+                boolean recipeFound = false;
+                for (RecipeHolder<?> holder : collection.getRecipes()) {
+                    String resultItemName = holder.value().getResultItem(minecraft.player.registryAccess()).getDisplayName().getString().toLowerCase(Locale.ROOT);
+                    if (resultItemName.contains(s)) recipeFound = true;
+                }
+                if (!recipeFound) list1.remove(collection);
+            }
+        }
+
+
+        if (book.isFiltering(this.menu)) {
+            list1.removeIf((collection) -> !collection.hasCraftable());
         }
 
         this.recipeBookPage.updateCollections(list1, resetPageNumber);
@@ -111,4 +160,24 @@ public class UpdateCollectionsMixin {
 //        }
     }
 
+    private static List<ItemStack> searchItems(String term) {
+        String namespace = getNamespace(term);
+        if (!namespace.isEmpty()) {
+            return BuiltInRegistries.ITEM.stream().filter(item ->{
+                return BuiltInRegistries.ITEM.getKey(item).toString().toLowerCase(Locale.ROOT).contains(term);
+            }).map(ItemStack::new).toList();
+        } else {
+            return BuiltInRegistries.ITEM.stream().filter(item ->{
+                return new ItemStack(item).getDisplayName().getString().toLowerCase(Locale.ROOT).contains(term);
+            }).map(ItemStack::new).toList();
+        }
+    }
+
+    private static String getNamespace(String str) {
+        if (str.indexOf(":", str.indexOf(":")+1) != -1 || str.indexOf(":")==-1) return "";
+        String testStr = str.split(":")[0];
+        Set<String> namespaces = BuiltInRegistries.ITEM.keySet().stream().map(ResourceLocation::getNamespace).collect(Collectors.toSet());
+        if (!namespaces.contains(testStr)) return "";
+        return String.valueOf((namespaces.stream().filter(ns->ns.equals(testStr)).findFirst()));
+    }
 }
