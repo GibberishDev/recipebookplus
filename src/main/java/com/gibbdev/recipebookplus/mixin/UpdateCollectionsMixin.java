845207b2a046d5b7ee9711b911664a1617d85893
GibberishDev <gibberishdevelopment@gmail.com>
2026-07-15 00:23:09 +0300

    Changed grouped recipes showing irrelevant recipes on search Example would be searching for "Oak" and getting "Jungle" stairs recipe in the group

--- src/main/java/com/gibbdev/recipebookplus/mixin/UpdateCollectionsMixin.java
+++ src/main/java/com/gibbdev/recipebookplus/mixin/UpdateCollectionsMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookPage;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookTabButton;
 import net.minecraft.client.gui.screens.recipebook.RecipeCollection;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -21,8 +22,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -35,29 +38,34 @@ import java.util.stream.Collectors;
 @Mixin(RecipeBookComponent.class)
 public abstract class UpdateCollectionsMixin {
 
-    private static boolean grouping = true;
+    @Unique
+    private static boolean rbp$grouping = true;
 
+    @Unique
     private static final WidgetSprites HELP_BUTTON = new WidgetSprites(
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/help"),
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/help"),
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/help"),
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/help"));
+    @Unique
     private static final WidgetSprites GROUP_BUTTON = new WidgetSprites(
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/group_enabled"),
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/group_disabled"),
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/group_enabled_hover"),
             ResourceLocation.fromNamespaceAndPath("recipebookplus","recipe_book/group_disabled_hover"));
 
-    private AbstractWidget helpButton;
-    private StateSwitchingButton groupButton;
+    @Unique
+    private AbstractWidget rbp$helpButton;
+    @Unique
+    private StateSwitchingButton rbp$groupButton;
 
-    @Shadow private EditBox searchBox;
+    @Shadow public EditBox searchBox;
     @Shadow private ClientRecipeBook book;
-    @Shadow private RecipeBookPage recipeBookPage;
+    @Final @Shadow private RecipeBookPage recipeBookPage;
     @Shadow private RecipeBookTabButton selectedTab;
-    @Shadow private StackedContents stackedContents;
-    @Shadow private RecipeBookMenu<?, ?> menu;
-    @Shadow private Minecraft minecraft;
+    @Final @Shadow private StackedContents stackedContents;
+    @Shadow protected RecipeBookMenu<?, ?> menu;
+    @Shadow protected Minecraft minecraft;
     @Shadow private int width;
     @Shadow private int height;
     @Shadow private int xOffset;
@@ -73,6 +81,8 @@ public abstract class UpdateCollectionsMixin {
     private void recipebookplus_updateCollections(boolean resetPageNumber,CallbackInfo ci) {
         if (!Config.MOD_ENABLED.getAsBoolean()) {return;}
         ci.cancel();
+        if (minecraft.level == null) return;
+        RegistryAccess ra = minecraft.level.registryAccess();
         List<RecipeCollection> list = book.getCollection(selectedTab.getCategory());
         list.forEach((collection) -> collection.canCraft(stackedContents, menu.getGridWidth(), menu.getGridHeight(), book));
         List<RecipeCollection> list1 = Lists.newArrayList(list);
@@ -82,14 +92,14 @@ public abstract class UpdateCollectionsMixin {
 
 
         List<RecipeCollection> tempList = Lists.newArrayList(list1);
-        if (!grouping) {
+        if (!rbp$grouping) {
             for (RecipeCollection collection : tempList) {
                 if (collection.getRecipes().size() >= 2) {
                     List<RecipeHolder<?>> holders = new ArrayList<>();
                     holders.addAll(collection.getDisplayRecipes(true));
                     holders.addAll(collection.getDisplayRecipes(false));
                     for (RecipeHolder<?> holder : holders) {
-                        RecipeCollection newCollection = new RecipeCollection(minecraft.level.registryAccess(), List.of(holder));
+                        RecipeCollection newCollection = new RecipeCollection(ra, List.of(holder));
                         newCollection.canCraft(stackedContents, menu.getGridWidth(), menu.getGridHeight(), book);
                         list1.add(list1.indexOf(collection), newCollection);
                     }
@@ -97,10 +107,10 @@ public abstract class UpdateCollectionsMixin {
                 }
             }
         }
-        if (s.startsWith(Config.INGREDIENT_PREFIX.get()) && s != Config.INGREDIENT_PREFIX.get()) {
+        if (s.startsWith(Config.INGREDIENT_PREFIX.get()) && !s.equals(Config.INGREDIENT_PREFIX.get())) {
             String searchTerm = s.replaceFirst(Matcher.quoteReplacement(Config.INGREDIENT_PREFIX.get()), "").strip();
-            if (searchTerm != "") {
-                List<ItemStack> itemStacks = searchItems(searchTerm);
+            if (!searchTerm.isEmpty()) {
+                List<ItemStack> itemStacks = rbp$searchItems(searchTerm);
                 tempList = Lists.newArrayList(list1);
                 for (RecipeCollection collection : tempList) {
                     boolean ingredientFound = false;
@@ -119,14 +129,44 @@ public abstract class UpdateCollectionsMixin {
                     }
                     if (!ingredientFound) list1.remove(collection);
                 }
+                tempList = Lists.newArrayList(list1);
+                for (RecipeCollection collection : tempList) {
+                    if (collection.getRecipes().size() >= 2) {
+                        List<RecipeHolder<?>> holders = new ArrayList<>();
+                        holders.addAll(collection.getDisplayRecipes(true));
+                        holders.addAll(collection.getDisplayRecipes(false));
+                        List<RecipeHolder<?>> holdersRelevant = new ArrayList<>();
+                        for (RecipeHolder<?> holder : holders) {
+                            Recipe<?> recipe = holder.value();
+                            for (Ingredient ingredient : recipe.getIngredients()) {
+                                boolean ingredientFound = false;
+                                for (ItemStack item : itemStacks) {
+                                    if (ingredient.test(item)) {
+                                        holdersRelevant.add(holder);
+                                        ingredientFound = true;
+                                        break;
+                                    }
+                                }
+                                if (ingredientFound) break;
+                            }
+                        }
+                        RecipeCollection newCollection = new RecipeCollection(ra, holdersRelevant);
+                        newCollection.canCraft(stackedContents, menu.getGridWidth(), menu.getGridHeight(), book);
+                        list1.add(list1.indexOf(collection), newCollection);
+                        list1.remove(collection);
+                    }
+                }
+
+
+
             }
-        } else if (s.startsWith(Config.MODID_PREFIX.get()) && s != Config.MODID_PREFIX.get()) {
+        } else if (s.startsWith(Config.MODID_PREFIX.get()) && !s.equals(Config.MODID_PREFIX.get())) {
             String searchTerm = s.replaceFirst(Matcher.quoteReplacement(Config.MODID_PREFIX.get()), "").strip();
-            if (searchTerm != "") {
+            if (!searchTerm.isEmpty()) {
                 tempList = Lists.newArrayList(list1);
                 for (RecipeCollection collection : tempList) {
                     for (RecipeHolder<?> holder : collection.getRecipes()) {
-                        Item resultItem = holder.value().getResultItem(minecraft.level.registryAccess()).getItem();
+                        Item resultItem = holder.value().getResultItem(ra).getItem();
                         String namespace = BuiltInRegistries.ITEM.getKey(resultItem).getNamespace();
                         if (!namespace.contains(searchTerm)) list1.remove(collection);
                     }
@@ -138,10 +178,31 @@ public abstract class UpdateCollectionsMixin {
             for (RecipeCollection collection : tempList) {
                 boolean recipeFound = false;
                 for (RecipeHolder<?> holder : collection.getRecipes()) {
-                    String resultItemName = holder.value().getResultItem(minecraft.level.registryAccess()).getDisplayName().getString().toLowerCase(Locale.ROOT);
-                    if (resultItemName.contains(s)) recipeFound = true;
+                    String resultItemName = holder.value().getResultItem(ra).getDisplayName().getString().toLowerCase(Locale.ROOT);
+                    if (resultItemName.contains(s)) {
+                        recipeFound = true;
+                    }
                 }
                 if (!recipeFound) list1.remove(collection);
+            }
+            tempList = Lists.newArrayList(list1);
+            for (RecipeCollection collection : tempList) {
+                if (collection.getRecipes().size() >= 2) {
+                    List<RecipeHolder<?>> holders = new ArrayList<>();
+                    holders.addAll(collection.getDisplayRecipes(true));
+                    holders.addAll(collection.getDisplayRecipes(false));
+                    List<RecipeHolder<?>> holdersRelevant = Lists.newArrayList(holders);
+                    for (RecipeHolder<?> holder : holders) {
+                        String resultItemName = holder.value().getResultItem(ra).getDisplayName().getString().toLowerCase(Locale.ROOT);
+                        if (!resultItemName.contains(s)) {
+                            holdersRelevant.remove(holder);
+                        }
+                    }
+                    RecipeCollection newCollection = new RecipeCollection(ra, holdersRelevant);
+                    newCollection.canCraft(stackedContents, menu.getGridWidth(), menu.getGridHeight(), book);
+                    list1.add(list1.indexOf(collection), newCollection);
+                    list1.remove(collection);
+                }
             }
         }
 
@@ -155,15 +216,15 @@ public abstract class UpdateCollectionsMixin {
      * Initialises aditional two ui elements - Help button (usage tooltip) and Group button
      * @Mixin {@link RecipeBookComponent#initVisuals()}
      */
-    @Inject(method = "initVisuals",at = @At("TAIL"),cancellable = false)
+    @Inject(method = "initVisuals",at = @At("TAIL"))
     public void recipebookplus_initVisuals(CallbackInfo ci) {
         if (Config.MOD_ENABLED.get()) {
             xOffset = widthTooNarrow ? 0 : 86;
             int i = (width - 147) / 2 - xOffset;
             int j = (height - 166) / 2;
             //I DONT CARE THAT I USE BUTTON FOR THE STATIC TEXTURE. SUE ME
-            helpButton = new ImageButton(i + 110,j + 139,26,16,HELP_BUTTON, fuck -> helpButton());
-            helpButton.setTooltip(Tooltip.create(Component.translatable(
+            this.rbp$helpButton = new ImageButton(i + 110,j + 139,26,16,HELP_BUTTON, fuck -> rbp$helpButton());
+            this.rbp$helpButton.setTooltip(Tooltip.create(Component.translatable(
                     "recipebookplus.gui.help_tooltip",
                     Component.literal(Config.INGREDIENT_PREFIX.get()).withStyle(ChatFormatting.GOLD),
                     Component.literal(Config.MODID_PREFIX.get()).withStyle(ChatFormatting.GOLD),
@@ -171,8 +232,8 @@ public abstract class UpdateCollectionsMixin {
                     Component.keybind("recipebookplus.keymapping.usage").withStyle(ChatFormatting.GREEN),
                     Component.keybind("recipebookplus.keymapping.mod").withStyle(ChatFormatting.GREEN)
             )));
-            groupButton = new StateSwitchingButton(i+11,j+139,26,16,true);
-            groupButton.initTextureValues(GROUP_BUTTON);
+            this.rbp$groupButton = new StateSwitchingButton(i+11,j+139,26,16, rbp$grouping);
+            this.rbp$groupButton.initTextureValues(GROUP_BUTTON);
         }
 
     }
@@ -181,11 +242,11 @@ public abstract class UpdateCollectionsMixin {
      * Injects right before rendering craftable filter toggle button to render 2 new windgets
      * @Mixin {@link RecipeBookComponent#render(GuiGraphics, int, int, float)}
      */
-    @Inject(method="render",at=@At(value="INVOKE",target="Lnet/minecraft/client/gui/components/StateSwitchingButton;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"),cancellable=false)
+    @Inject(method="render",at=@At(value="INVOKE",target="Lnet/minecraft/client/gui/components/StateSwitchingButton;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V"))
     public void recipebookplus_render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
         if (Config.MOD_ENABLED.get()) {
-            helpButton.render(guiGraphics,mouseX,mouseY,partialTick);
-            groupButton.render(guiGraphics,mouseX,mouseY,partialTick);
+            this.rbp$helpButton.render(guiGraphics,mouseX,mouseY,partialTick);
+            this.rbp$groupButton.render(guiGraphics,mouseX,mouseY,partialTick);
         }
     }
 
@@ -197,9 +258,9 @@ public abstract class UpdateCollectionsMixin {
     @Inject(method="mouseClicked",at=@At(value = "INVOKE",target = "Lnet/minecraft/client/gui/components/EditBox;setFocused(Z)V"),cancellable = true)
     public void mouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
         if (Config.MOD_ENABLED.get()) {
-            if (this.groupButton.mouseClicked(mouseX, mouseY, button)) {
-                this.toggleGrouping();
-                this.groupButton.setStateTriggered(grouping);
+            if (this.rbp$groupButton.mouseClicked(mouseX, mouseY, button)) {
+                this.rbp$toggleGrouping();
+                this.rbp$groupButton.setStateTriggered(rbp$grouping);
                 recipebookplus_updateCollections(true, new CallbackInfo("updateCollections", true));
                 cir.setReturnValue(true);
             }
@@ -211,21 +272,13 @@ public abstract class UpdateCollectionsMixin {
 //region misc methods
 
 
-    /**
-     *
-     * @param term
-     * @return
-     */
-    private static List<ItemStack> searchItems(String term) {
-        String namespace = getNamespace(term);
+    @Unique
+    private static List<ItemStack> rbp$searchItems(String term) {
+        String namespace = rbp$getNamespace(term);
         if (!namespace.isEmpty()) {
-            return BuiltInRegistries.ITEM.stream().filter(item ->{
-                return BuiltInRegistries.ITEM.getKey(item).toString().toLowerCase(Locale.ROOT).contains(term);
-            }).map(ItemStack::new).toList();
+            return BuiltInRegistries.ITEM.stream().filter(item -> BuiltInRegistries.ITEM.getKey(item).toString().toLowerCase(Locale.ROOT).contains(term)).map(ItemStack::new).toList();
         } else {
-            return BuiltInRegistries.ITEM.stream().filter(item ->{
-                return new ItemStack(item).getDisplayName().getString().toLowerCase(Locale.ROOT).contains(term);
-            }).map(ItemStack::new).toList();
+            return BuiltInRegistries.ITEM.stream().filter(item -> new ItemStack(item).getDisplayName().getString().toLowerCase(Locale.ROOT).contains(term)).map(ItemStack::new).toList();
         }
     }
 
@@ -234,28 +287,25 @@ public abstract class UpdateCollectionsMixin {
      * @param str {@link String} input string
      * @return {@link  String} outputs valid namespace or empty string ("") if no fitting namesspaces were found
      */
-    private static String getNamespace(String str) {
-        if (str.indexOf(":", str.indexOf(":")+1) != -1 || str.indexOf(":")==-1) return "";
+    @Unique
+    private static String rbp$getNamespace(String str) {
+        if (str.indexOf(":", str.indexOf(":")+1) != -1 || !str.contains(":")) return "";
         String testStr = str.split(":")[0];
         Set<String> namespaces = BuiltInRegistries.ITEM.keySet().stream().map(ResourceLocation::getNamespace).collect(Collectors.toSet());
         if (!namespaces.contains(testStr)) return "";
         return String.valueOf((namespaces.stream().filter(ns->ns.equals(testStr)).findFirst()));
     }
 
-    /**Dud method to plug in the requirement for callback for {@link ImageButton} {@link UpdateCollectionsMixin#helpButton}*/
-    private void helpButton() {return;}
+    /**Dud method to plug in the requirement for callback for {@link ImageButton} {@link UpdateCollectionsMixin#rbp$helpButton}*/
+    @Unique
+    private void rbp$helpButton() {}
 
-    /**Updates grouping of Recipe Collections and updates visuals for {@link UpdateCollectionsMixin#groupButton}*/
-    private void toggleGrouping() {
-        grouping = !grouping;
-        groupButton.setTooltip(grouping ? Tooltip.create(Component.translatable("recipebookplus.gui.grouping")) : Tooltip.create(Component.translatable("recipebookplus.gui.not_grouping")));
-        groupButton.initTextureValues(GROUP_BUTTON);
-    }
-
-    /**@ToDo: implement keybinds for entering search temrs based on the hovered item JEI style*/
-    public void search(String searchTerm) {
-        searchBox.setValue(searchTerm);
-        recipebookplus_updateCollections(true, new CallbackInfo("updateCollections", true));
+    /**Updates grouping of Recipe Collections and updates visuals for {@link UpdateCollectionsMixin#rbp$groupButton}*/
+    @Unique
+    private void rbp$toggleGrouping() {
+        rbp$grouping = !rbp$grouping;
+        rbp$groupButton.setTooltip(rbp$grouping ? Tooltip.create(Component.translatable("recipebookplus.gui.grouping")) : Tooltip.create(Component.translatable("recipebookplus.gui.not_grouping")));
+        rbp$groupButton.initTextureValues(GROUP_BUTTON);
     }
 
 //endreion
